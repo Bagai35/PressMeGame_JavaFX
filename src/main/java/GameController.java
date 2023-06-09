4c9803a95e4ca01ad6d78cb560a0dcbe86f870f6
bagai35 <bogdan.keplast@gmail.com>
2023-06-09 10:08:18 +0300

    first commit

--- src/main/java/GameController.java
+++ src/main/java/GameController.java
@@ -1,91 +1,82 @@
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.util.Duration;
 
-public class GameController {
+import java.net.URL;
+import java.util.Random;
+import java.util.ResourceBundle;
+
+public class GameController implements Initializable {
 
     @FXML
     private Button button;
-
     @FXML
     private Label scoreLabel;
-
     @FXML
-    private Label highScoreLabel;
-
-    @FXML
-    private Button newGameButton;
+    private Label bestScoreLabel;
 
     private int score;
-    private int highScore;
-
+    private int bestScore;
+    private Random random;
     private Timeline timeline;
 
-    public void initialize() {
-        score = 0;
-        highScore = 0;
-        scoreLabel.setText("Score: 0");
-        highScoreLabel.setText("High Score: 0");
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        random = new Random();
+        bestScore = 0;
+        updateScoreLabel();
+        updateBestScoreLabel();
 
-        button.setOnAction(event -> {
-            increaseScore();
-            updateScoreLabel();
-            increaseButtonSpeed();
-        });
-
-        newGameButton.setOnAction(event -> startNewGame());
-
-        startNewGame();
-    }
-    @FXML
-    private void startNewGame() {
-        score = 0;
-        scoreLabel.setText("Score: 0");
-        hideNewGameButton();
+        button.setOnAction(this::handleButtonClick);
         startGame();
     }
 
     private void startGame() {
-        timeline = new Timeline(new KeyFrame(Duration.millis(100), event -> moveButton()));
-        timeline.setCycleCount(Timeline.INDEFINITE);
+        score = 0;
+        updateScoreLabel();
+        button.setLayoutX(random.nextDouble() * 400);
+        button.setLayoutY(random.nextDouble() * 400);
+        button.setDisable(false);
+
+        if (timeline != null) {
+            timeline.stop();
+        }
+
+        timeline = new Timeline(new KeyFrame(Duration.seconds(15), this::handleGameEnd));
         timeline.play();
     }
 
-    private void stopGame() {
-        timeline.stop();
-    }
+    private void handleButtonClick(ActionEvent event) {
+        score++;
+        updateScoreLabel();
 
-    private void moveButton() {
-        double newX = Math.random() * 450;
-        double newY = Math.random() * 450;
+        if (score > bestScore) {
+            bestScore = score;
+            updateBestScoreLabel();
+        }
+
+        double newX = random.nextDouble() * 400;
+        double newY = random.nextDouble() * 400;
         button.setLayoutX(newX);
         button.setLayoutY(newY);
+        button.setDisable(false);
     }
 
-    private void increaseScore() {
-        score++;
-        if (score > highScore) {
-            highScore = score;
-            highScoreLabel.setText("High Score: " + highScore);
-        }
+    private void handleGameEnd(ActionEvent event) {
+        button.setDisable(true);
+        timeline.stop();
     }
 
     private void updateScoreLabel() {
         scoreLabel.setText("Score: " + score);
     }
 
-    private void increaseButtonSpeed() {
-        timeline.setRate(timeline.getRate() + 0.1);
-    }
-
-    private void showNewGameButton() {
-        newGameButton.setVisible(true);
-    }
-
-    private void hideNewGameButton() {
-        newGameButton.setVisible(false);
+    private void updateBestScoreLabel() {
+        bestScoreLabel.setText("Best Score: " + bestScore);
     }
 }
